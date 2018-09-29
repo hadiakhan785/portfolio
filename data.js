fa803ca56637cd817c903a01532494a8163d2923
hadiakhan785 <hadiakhan785@gmail.com>
2018-09-29 12:05:06 +0500

    initial

---
+++ data.js
@@ -0,0 +1,30 @@
+let projects = [
+    {
+        img: "calculator",
+        url: "https://hadiakhan785.github.io/calculator/"
+    },
+    {
+        img: "cricket-project",
+        url: "https://hadiakhan785.github.io/cricket-site/"
+    },
+    {
+        img: "dancing-circles",
+        url: "https://hadiakhan785.github.io/dancing-circles/"
+    },
+    {
+        img: "exhibition-site",
+        url: "https://hadiakhan785.github.io/making-life-matters/"
+    },
+    {
+        img: "multi-function-calc",
+        url: "https://hadiakhan785.github.io/multi-function-calculator/"
+    },
+    {
+        img: "qoutes-app",
+        url: "https://hadiakhan785.github.io/quotes/"
+    },
+    {
+        img: "weather",
+        url: "https://hadiakhan785.github.io/weather-app-2/"
+    }
+];
---
+++ script.js
@@ -0,0 +1,33 @@
+function initialize() {
+    // document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    //     anchor.addEventListener('click', function (e) {
+    //         e.preventDefault();
+    
+    //         document.querySelector(this.getAttribute('href')).scrollIntoView({
+    //             behavior: 'smooth'
+    //         });
+    //     });
+    // });
+    
+
+      $(document).scroll(function() {
+        $(".html").animate({ width: "85%" }, 3000);
+        $(".css").animate({ width: "80%" }, 3000);
+        $(".js").animate({ width: "75%" }, 3000);
+        $(".bootstrap").animate({ width: "70%" }, 3000);
+        $(".jquery").animate({ width: "70%" }, 3000);
+      });
+
+      let projectContainer = document.querySelector(".project-container");
+      for(let i = 0; i < projects.length; i++){
+        projectContainer.innerHTML += `
+          <div class="project-item" data-aos="flip-left" data-aos-offset="200" data-aos-delay="50" data-aos-easing="ease-out-cubic"
+          data-aos-duration="2000" data-aos-mirror="true" data-aos-once="false">
+            <div class="project-img" style="background-image: url('${projects[i].img}.png')" ></div>
+            <div class="project-url">
+              <a href="${projects[i].url}" target="_blank">Project</a>
+            </div>
+          </div>
+        `;
+      }
+  }

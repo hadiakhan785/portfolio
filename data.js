dc143f2e7eb1e02c5f7434a362d7002348121e31
hadiakhan785 <38943097+hadiakhan785@users.noreply.github.com>
2018-09-29 14:26:07 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,17 +1,21 @@
 let projects = [
     {
+        name:"Calculator",
         img: "calculator",
         url: "https://hadiakhan785.github.io/calculator/"
     },
     {
+        name:"Cricket Project",
         img: "cricket-project",
         url: "https://hadiakhan785.github.io/cricket-site/"
     },
     {
+        name:"Dancing Circles",
         img: "dancing-circles",
         url: "https://hadiakhan785.github.io/dancing-circles/"
     },
     {
+        name:"Exhibitiob Site",
         img: "exhibition-site",
         url: "https://hadiakhan785.github.io/making-life-matters/"
     },
@@ -20,10 +24,12 @@ let projects = [
         url: "https://hadiakhan785.github.io/multi-function-calculator/"
     },
     {
+        name:"Quotes App",
         img: "qoutes-app",
         url: "https://hadiakhan785.github.io/quotes/"
     },
     {
+        name:"Weather App",
         img: "weather",
         url: "https://hadiakhan785.github.io/weather-app-2/"
     }

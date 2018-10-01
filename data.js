fd332ad874ab5ca89529fc355eab7b6d02f22db4
hadiakhan785 <hadiakhan785@gmail.com>
2018-10-01 21:39:18 +0500

    commit

--- data.js
+++ data.js
@@ -2,36 +2,43 @@ let projects = [
     {
         name:"Calculator",
         img: "calculator",
-        url: "https://hadiakhan785.github.io/calculator/"
+        url: "https://hadiakhan785.github.io/calculator/",
+        repoUrl: "https://github.com/hadiakhan785/calculator"
     },
     {
         name:"Cricket Project",
         img: "cricket-project",
-        url: "https://hadiakhan785.github.io/cricket-site/"
+        url: "https://hadiakhan785.github.io/cricket-site/",
+        repoUrl: "https://github.com/hadiakhan785/cricket-site"
     },
     {
         name:"Dancing Circles",
         img: "dancing-circles",
-        url: "https://hadiakhan785.github.io/dancing-circles/"
+        url: "https://hadiakhan785.github.io/dancing-circles/",
+        repoUrl: "https://github.com/hadiakhan785/dancing-circles"
     },
     {
         name:"Exhibitiob Site",
         img: "exhibition-site",
-        url: "https://hadiakhan785.github.io/making-life-matters/"
+        url: "https://hadiakhan785.github.io/making-life-matters/",
+        repoUrl: "https://github.com/hadiakhan785/making-life-matters"
     },
     {
-        name: "Calculator",
+        name: "Multi-Function Calculator",
         img: "multi-function-calc",
-        url: "https://hadiakhan785.github.io/multi-function-calculator/"
+        url: "https://hadiakhan785.github.io/multi-function-calculator/",
+        repoUrl: "https://github.com/hadiakhan785/multi-function-calculator"
     },
     {
         name:"Quotes App",
         img: "qoutes-app",
-        url: "https://hadiakhan785.github.io/quotes/"
+        url: "https://hadiakhan785.github.io/quotes/",
+        repoUrl: "https://github.com/hadiakhan785/quotes"
     },
     {
         name:"Weather App",
         img: "weather",
-        url: "https://hadiakhan785.github.io/weather-app-2/"
+        url: "https://hadiakhan785.github.io/weather-app-2/",
+        repoUrl: "https://github.com/hadiakhan785/weather-app-2"
     }
 ];
